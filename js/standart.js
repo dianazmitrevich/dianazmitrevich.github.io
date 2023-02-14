0a8faa8ac9b2ae7d9a62c0125cf7f37f9d25b4c8
d <d@MacBook-Pro-d.local>
2023-02-14 23:07:53 +0300

    Add markup

---
+++ js/standart.js
@@ -0,0 +1,12 @@
+'use strict';
+
+$(".card__slider").slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 1200,
+    slidesToShow: 1,
+    centerMode: false,
+    variableWidth: true,
+});
